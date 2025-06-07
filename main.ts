424b7871fcdb53d0829400d15c80754467e08f0a
mpstaton <mpstaton@gmail.com>
2025-06-07 19:20:42 +0300

    upgrade(packages): all packages are now up to date
     On branch development
     Changes to be committed:
    	modified:   .eslintrc
    	modified:   esbuild.config.mjs
    	modified:   main.ts
    	modified:   package.json
    	modified:   pnpm-lock.yaml
    	modified:   tsconfig.json

--- main.ts
+++ main.ts
@@ -1,134 +1,150 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import type { App, Editor } from 'obsidian';
+import { MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface ContentFarmSettings {
+    mySetting: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: ContentFarmSettings = {
+    mySetting: 'default'
+};
+
+export default class ContentFarmPlugin extends Plugin {
+    public settings: ContentFarmSettings = DEFAULT_SETTINGS;
+    private statusBarItemEl: HTMLElement | null = null;
+    private ribbonIconEl: HTMLElement | null = null;
+
+    async onload(): Promise<void> {
+        await this.loadSettings();
+
+        this.registerRibbonIcon();
+        this.setupStatusBar();
+        this.registerCommands();
+        this.addSettingTab(new ContentFarmSettingTab(this.app, this));
+
+        this.registerDomEvent(document, 'click', this.handleDocumentClick);
+    }
+
+    onunload(): void {
+        this.statusBarItemEl?.remove();
+        this.ribbonIconEl?.remove();
+    }
+
+    private async loadSettings(): Promise<void> {
+        this.settings = { 
+            ...DEFAULT_SETTINGS, 
+            ...(await this.loadData() ?? {}) 
+        };
+    }
+
+    public async saveSettings(): Promise<void> {
+        try {
+            await this.saveData(this.settings);
+        } catch (error) {
+            console.error('Failed to save settings:', error);
+            new Notice('Failed to save settings');
+        }
+    }
+
+    private registerRibbonIcon(): void {
+        this.ribbonIconEl = this.addRibbonIcon(
+            'dice', 
+            'Content Farm', 
+            () => {
+                new Notice('This is Deep Lossless notice!');
+            }
+        );
+        this.ribbonIconEl?.addClass('content-farm-ribbon');
+    }
+
+    private setupStatusBar(): void {
+        this.statusBarItemEl = this.addStatusBarItem();
+        if (this.statusBarItemEl) {
+            this.statusBarItemEl.setText('Content Farm Active');
+        }
+    }
+
+    private registerCommands(): void {
+        this.addCommand({
+            id: 'open-content-farm-modal',
+            name: 'Open Content Farm',
+            callback: () => {
+                new ContentFarmModal(this.app).open();
+            }
+        });
+
+        this.addCommand({
+            id: 'insert-sample-text',
+            name: 'Insert Sample Text',
+            editorCallback: (editor: Editor) => {
+                editor.replaceSelection('Sample text from Content Farm');
+            }
+        });
+
+        this.addCommand({
+            id: 'open-sample-modal-complex',
+            name: 'Open sample modal (complex)',
+            checkCallback: (checking: boolean) => {
+                const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+                if (markdownView) {
+                    if (!checking) {
+                        new ContentFarmModal(this.app).open();
+                    }
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private readonly handleDocumentClick = (evt: MouseEvent): void => {
+        if (process.env.NODE_ENV === 'development') {
+            console.debug('Document click:', evt);
+        }
+    };
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+class ContentFarmModal extends Modal {
+    constructor(app: App) {
+        super(app);
+    }
 
-	async onload() {
-		await this.loadSettings();
+    onOpen(): void {
+        const { contentEl } = this;
+        contentEl.createEl('h2', { text: 'Content Farm' });
+        contentEl.createEl('p', { text: 'Welcome to Content Farm Plugin!' });
+    }
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Content Farm', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is Lossless notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open new modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+    onClose(): void {
+        const { contentEl } = this;
+        contentEl.empty();
+    }
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class ContentFarmSettingTab extends PluginSettingTab {
+    plugin: ContentFarmPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+    constructor(app: App, plugin: ContentFarmPlugin) {
+        super(app, plugin);
+        this.plugin = plugin;
+    }
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+    display(): void {
+        const { containerEl } = this;
+        containerEl.empty();
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+        containerEl.createEl('h2', { text: 'Content Farm Settings' });
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+        new Setting(containerEl)
+            .setName('Custom Setting')
+            .setDesc('Configure your content farm settings')
+            .addText(text => text
+                .setPlaceholder('Enter your setting')
+                .setValue(this.plugin.settings.mySetting)
+                .onChange(async (value: string) => {
+                    this.plugin.settings.mySetting = value;
+                    await this.plugin.saveSettings();
+                })
+            );
+    }
 }
